1e0f0f14112174d5e5bbcb5eeb73eed301b92930
ayush kumar <kumarayush9527@gmail.com>
2024-11-04 16:30:25 +0530

    first question added

---
+++ question1.js
@@ -0,0 +1,14 @@
+//  Write a function that takes two numbers as arguments and returns their sum
+
+
+function sumNumbers(num1, num2) {
+ 
+  const sum = num1 + num2;
+  
+  return sum;
+}
+
+
+console.log(sumNumbers(5, 10));
+
+// Output: 15
