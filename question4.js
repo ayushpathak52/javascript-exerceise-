c4a81a1bced372fcaa047afb8a1dcdddb9cac709
ayush kumar <kumarayush9527@gmail.com>
2024-11-05 15:44:40 +0530

    question 4 added

---
+++ question4.js
@@ -0,0 +1,17 @@
+ // Write a function that takes two numbers as arguments and returns the larger number.
+
+ 
+function findLargerNumber(num1, num2) {
+
+  if (num1 > num2) {
+    
+    return num1;
+  } else {
+    
+    return num2;
+  }
+}
+
+console.log(findLargerNumber(5, 10)); 
+
+// Output: 10
